e029da9a07de2f51937a9971e568549d0ad7aac1
Maahirgit <Maahirgit@gmail.com>
2026-04-08 13:01:55 +0530

    Implemented basic inventory stock checking logic

---
+++ src/patterns/strategy/StandardPricing.h
@@ -0,0 +1,8 @@
+#include "InventorySystem.h"
+#include <iostream>
+using namespace std;
+
+void InventorySystem::checkStock(string item) {
+    cout << "Checking stock for: " << item << endl;
+    cout << "Item Available\n";
+}
