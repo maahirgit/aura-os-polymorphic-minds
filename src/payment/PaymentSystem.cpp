510154b01eeaf6d868df405c67e5702a2c8c5291
Rishikashah2674 <rishikashah2674@gmail.com>
2026-04-10 15:07:20 +0530

    Implements basic payment processing logic

---
+++ src/payment/PaymentSystem.cpp
@@ -0,0 +1,8 @@
+#include "PaymentSystem.h"
+#include <iostream>
+using namespace std;
+
+void PaymentSystem::processPayment(int amount) {
+    cout << "Processing Payment: " << amount << endl;
+    cout << "Payment Successful\n";
+}
