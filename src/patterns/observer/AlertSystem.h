b2b709ec0594f9d4cc24b7e0f4e08410de675d62
Meet Sheth <mitsheth2@gmail.com>
2026-04-12 18:45:09 +0530

    Implement AlertSystem class for observer pattern

--- src/patterns/observer/AlertSystem.h
+++ src/patterns/observer/AlertSystem.h
@@ -1,1 +1,15 @@
+#ifndef ALERT_SYSTEM_H
+#define ALERT_SYSTEM_H
 
+#include "Observer.h"
+#include <iostream>
+using namespace std;
+
+class AlertSystem : public Observer {
+public:
+    void update(string message) override {
+        cout << "🔔 ALERT: " << message << endl;
+    }
+};
+
+#endif
