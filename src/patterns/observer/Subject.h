60e976d8b272c8a6d847aee146daba82b7ed3601
Rishikashah2674 <rishikashah2674@gmail.com>
2026-04-12 18:48:48 +0530

    Implemented subject for observer pattern

--- src/patterns/observer/Subject.h
+++ src/patterns/observer/Subject.h
@@ -1,1 +1,22 @@
+#ifndef SUBJECT_H
+#define SUBJECT_H
 
+#include <vector>
+#include "Observer.h"
+using namespace std;
+
+class Subject {
+    vector<Observer*> observers;
+
+public:
+    void addObserver(Observer* o) {
+        observers.push_back(o);
+    }
+
+    void notify(string msg) {
+        for(auto o : observers)
+            o->update(msg);
+    }
+};
+
+#endif
