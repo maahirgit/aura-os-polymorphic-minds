e5bba91af970c60ce5c3bebcb0134ce57d75bef8
krishadoshi16 <202512112@dau.ac.in>
2026-04-13 09:40:04 +0530

    Implemented factory for dynamic system creation

--- src/patterns/factory/SystemFactory.h
+++ src/patterns/factory/SystemFactory.h
@@ -1,1 +1,15 @@
+#ifndef SYSTEM_FACTORY_H
+#define SYSTEM_FACTORY_H
 
+#include "../../inventory/InventorySystem.h"
+#include <string>
+using namespace std;
+
+class SystemFactory {
+public:
+    static InventorySystem createSystem(string type) {
+        return InventorySystem(type);
+    }
+};
+
+#endif
